e01985c482424231e0ac7c05d5f78c818d559378
ShiJi <ShiJiVK@outlook.com>
2023-05-25 15:20:49 +0800

    add timer

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <chrono>
 
 struct TimestampedImage {
     double timestamp;
@@ -41,7 +42,12 @@ int main(){
     std::vector<TimestampedImage> rgb_images = read_timestamps_and_filenames(base_path + "rgb.txt");
     std::cout << "Image sequences found." << std::endl;
 
-    for (size_t i = 0; i < depth_images.size() && i < rgb_images.size(); ++i) {
+    // 开始计时
+    auto start = std::chrono::high_resolution_clock::now();
+
+    size_t i = 0;
+
+    for (i = 0; i < depth_images.size() && i < rgb_images.size(); ++i) {
         // 读取深度图像
         cv::Mat depth_image_16U = cv::imread(base_path + depth_images[i].filename, cv::IMREAD_UNCHANGED);
         cv::Mat rgb_image = cv::imread(base_path + rgb_images[i].filename, cv::IMREAD_COLOR);
@@ -59,9 +65,27 @@ int main(){
         }
     }
 
-    std::cout << "All images processed, extracting point cloud, please wait." << std::endl;
+
+
+    // 结束计时
+    auto end = std::chrono::high_resolution_clock::now();
+
+    // 计算执行时间
+    std::chrono::duration<double> duration = end - start;
+
+    // 将执行时间转换为毫秒
+    double milliseconds = duration.count() * 1000;
+
+    // 输出执行时间
+    std::cout << "重建总消耗时间: " << milliseconds << " 毫秒" << std::endl;
+    // 计算平均每帧时间花费
+    std::cout << "平均每帧花费时间: " << milliseconds * 1.0 / (i + 1) << " 毫秒" << std::endl;
+    //FPS
+    std::cout << "每秒平均生成帧: " << 1000.0 / (milliseconds * 1.0 / (i + 1))<< "帧" << std::endl;
+
+    std::cout << "已处理完所有图片，正在导出点云" << std::endl;
     scheduler.extract_and_save_pointcloud();
-    std::cout << "Point clound saved in ./output.pcd." << std::endl;
+    std::cout << "点云已保存至output.pcd 和 output.ply" << std::endl;
     system("pause");
 
     return 0;
