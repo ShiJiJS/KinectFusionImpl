b5e3a588417a2baceae008080adba94217c7e33d
ShiJi <ShiJiVK@outlook.com>
2023-05-30 18:21:42 +0800

    support ICL-NIUM

--- src/main.cpp
+++ src/main.cpp
@@ -32,36 +32,88 @@ std::vector<TimestampedImage> read_timestamps_and_filenames(const std::string& f
     return result;
 }
 
+struct AssociatedImages {
+    std::string depth_filename;
+    std::string rgb_filename;
+};
+
+std::vector<AssociatedImages> read_associations(const std::string& filepath) {
+    std::vector<AssociatedImages> result;
+    std::ifstream file(filepath);
+    std::string line;
+
+    while (std::getline(file, line)) {
+        if (line.empty() || line[0] == '#') {
+            continue;
+        }
+
+        std::istringstream iss(line);
+        std::string discard;
+        AssociatedImages entry;
+
+        // Assuming the format is: ID depth_filename ID rgb_filename
+        iss >> discard >> entry.depth_filename >> discard >> entry.rgb_filename;
+
+        result.push_back(entry);
+    }
+
+    return result;
+}
+
+
 
 
 int main(){
     Scheduler scheduler = SchedulerFactory::build();
-
-    std::string base_path = "./";
-    std::vector<TimestampedImage> depth_images = read_timestamps_and_filenames(base_path + "depth.txt");
-    std::vector<TimestampedImage> rgb_images = read_timestamps_and_filenames(base_path + "rgb.txt");
-    std::cout << "Image sequences found." << std::endl;
-
     // 开始计时
     auto start = std::chrono::high_resolution_clock::now();
-
     size_t i = 0;
 
-    for (i = 0; i < depth_images.size() && i < rgb_images.size(); ++i) {
-        // 读取深度图像
-        cv::Mat depth_image_16U = cv::imread(base_path + depth_images[i].filename, cv::IMREAD_UNCHANGED);
-        cv::Mat rgb_image = cv::imread(base_path + rgb_images[i].filename, cv::IMREAD_COLOR);
+    std::string base_path = "./";
+
+
+    //Tum数据集
+    //std::vector<TimestampedImage> depth_images = read_timestamps_and_filenames(base_path + "depth.txt");
+    //std::vector<TimestampedImage> rgb_images = read_timestamps_and_filenames(base_path + "rgb.txt");
+    //std::cout << "Image sequences found." << std::endl;
+    // for (i = 0; i < depth_images.size() && i < rgb_images.size(); ++i) {
+    //     // 读取深度图像
+    //     cv::Mat depth_image_16U = cv::imread(base_path + depth_images[i].filename, cv::IMREAD_UNCHANGED);
+    //     cv::Mat rgb_image = cv::imread(base_path + rgb_images[i].filename, cv::IMREAD_COLOR);
+
+    //     if (depth_image_16U.empty() || rgb_image.empty()) {
+    //         std::cerr << "Failed to load image: " << depth_images[i].filename << " or " << rgb_images[i].filename << std::endl;
+    //         continue;
+    //     }
+        
+    //     //std::cout << "Successfully read image " << i << "." << std::endl;
+    //     bool success = scheduler.process_new_frame(depth_image_16U, rgb_image);
+
+    //     if (!success) {
+    //         std::cerr << "Failed to process frame: " << depth_images[i].filename << " and " << rgb_images[i].filename << std::endl;
+    //     }
+    // }
+
+    // ICL-NIUM数据集
+    std::vector<AssociatedImages> images = read_associations(base_path + "associations.txt");
+    std::cout << "Image sequences found." << std::endl;
+
+    for (size_t i = 0; i < images.size(); ++i) {
+        // 读取深度图像和RGB图像
+        cv::Mat depth_image_16U = cv::imread(base_path + images[i].depth_filename, cv::IMREAD_UNCHANGED);
+        cv::Mat rgb_image = cv::imread(base_path + images[i].rgb_filename, cv::IMREAD_COLOR);
 
         if (depth_image_16U.empty() || rgb_image.empty()) {
-            std::cerr << "Failed to load image: " << depth_images[i].filename << " or " << rgb_images[i].filename << std::endl;
+            std::cerr << "Failed to load image: " << images[i].depth_filename << " or " << images[i].rgb_filename << std::endl;
             continue;
         }
-        
+
+        // Process the frame
         std::cout << "Successfully read image " << i << "." << std::endl;
         bool success = scheduler.process_new_frame(depth_image_16U, rgb_image);
 
         if (!success) {
-            std::cerr << "Failed to process frame: " << depth_images[i].filename << " and " << rgb_images[i].filename << std::endl;
+            std::cerr << "Failed to process frame: " << images[i].depth_filename << " and " << images[i].rgb_filename << std::endl;
         }
     }
 
@@ -86,7 +138,6 @@ int main(){
     std::cout << "已处理完所有图片，正在导出点云" << std::endl;
     scheduler.extract_and_save_pointcloud();
     std::cout << "点云已保存至output.pcd 和 output.ply" << std::endl;
-    system("pause");
 
     return 0;
 }
